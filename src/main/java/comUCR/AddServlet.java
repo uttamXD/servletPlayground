5eb06e15a0281a5f522e84246281977cf6572d68
uttam rai <raiuttam88.ur@gmail.com>
2024-12-20 00:18:01 +0545

    servlet

---
+++ src/main/java/comUCR/AddServlet.java
@@ -0,0 +1,19 @@
+package comUCR;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AddServlet extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int i = Integer.parseInt(req.getParameter("num1"));
+        int j = Integer.parseInt(req.getParameter("num2"));
+
+        int k = i + j;
+
+        PrintWriter out = res.getWriter();
+        out.println("Result is: " + k);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <form action="add">
+        Enter first number: <input type="text" name="num1"><br>
+        Enter second number: <input type="text" name="num2"><br>
+        <input type="Submit">
+    </form>
+</body>
+</html>
